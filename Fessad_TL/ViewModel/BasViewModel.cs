bfd7cde2b64d872ae9c2dec296a8e793663f3404
ABADY1000 <abady1000@gmail.com>
2018-08-02 13:04:41 +0300

    Add project files.

---
+++ Fessad_TL/ViewModel/BasViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Fessad_TL.Annotations;
+using PropertyChanged;
+
+namespace Fessad_TL
+{
+    [ImplementPropertyChanged]
+    internal class BasViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged = (sender, x) => { };
+
+        public void OnPropertyChanged(string name)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+        
+    }
+}
